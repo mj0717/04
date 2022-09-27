45d20055314587a39f2875e1f639f6e7a45ef749
mj0717 <a81930875@gmail.com>
2022-09-27 17:25:10 +0900

    4-4

--- main.c
+++ main.c
@@ -4,16 +4,20 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int x, y;
+	int x;
 	
-	printf("input two integers: ");
-	scanf("%i, %i",&x, &y);
+	printf("input the year: ");
+	scanf("%i", &x);
 	
-	printf("+ result is %i\n", x+y);
-	printf("- result is %i\n", x-y);
-	printf("* result is %i\n", x*y);
-	printf("/ result is %i\n", x/y);
-	printf("%% result is %i\n", x%y);
+	if((x%4==0&&x%100!=0)||x%400==0)
+	{	
+		printf("%i is leap year!\n", x);
+	}
+	else
+	{
+		printf("%i is not leap year!\n", x);
+	}
+	  
 	
 	
 	 
